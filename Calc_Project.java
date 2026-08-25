3e81ff9a548c24f1a17a6865e43dce64b4c3718d
Sunaina <Sunaina@gmail.com>
2026-08-25 11:19:48 +0530

    calc_project file modified

--- Calc_Project.java
+++ Calc_Project.java
@@ -1,6 +1,10 @@
 class Calc_Project{
 public int add(int a, int b) {
+int c = a + b;
+return c;
 }
 public static void main(String[] args) {
+Calc_Project cal = new Calc_Project();
+System.out.println("The sum is: " + cal.add(2,4));
 }
 }
